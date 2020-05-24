598774660fd195214c545a416644a302faed97a4
Brian Levin <bml201095@gmail.com>
2020-05-24 18:40:21 -0400

    created the object relational mapping variable which will manage the database tables to classes and instances of classes to rows in those tables

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -1,1 +1,12 @@
 const connection = require('./connection.js');
+
+var orm = {
+    selectAll: function (tableName, cb) {
+        connection.query('SELECT * FROM ' + tableName, function (err, res) {
+            if (err) {
+                throw err;
+            } else {
+                cb(res)
+            }
+        });
+    },
