34a2def3e48004d69a46f98394fc8922991131c7
Brian Levin <bml201095@gmail.com>
2020-05-24 18:05:37 -0400

    created the connection variable which is going to create the connection to the my sql server

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,1 +1,9 @@
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "Password123!",
+    database: "burger_eat"
+});
