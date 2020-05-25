e35540e5deeed14dc39285f5f0b11742081ba3e9
Brian Levin <bml201095@gmail.com>
2020-05-25 00:17:33 -0400

    created an update function which will update the new burger values if true

--- burger/controllers/burger_controllers.js
+++ burger/controllers/burger_controllers.js
@@ -23,3 +23,15 @@ router.post("/api/burger", function (req, res) {
         res.json({ id: result.insertId });
     });
 });
+
+burger.update({
+    devoured: true,
+}, condition, function (result) {
+    if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+    } else {
+        res.status(200).end();
+    }
+});
+});
