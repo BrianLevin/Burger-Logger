e33903cd8f722ba10fdb3a72f8cb93c7a8776d87
Brian Levin <bml201095@gmail.com>
2020-05-25 00:09:37 -0400

    added a git fitfunction which will bind the object property to the function

--- burger/controllers/burger_controllers.js
+++ burger/controllers/burger_controllers.js
@@ -2,3 +2,13 @@ var burgers = require('../models/burger.js');
 var express = require('express');
 
 var router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
