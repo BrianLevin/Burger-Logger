d66170e1e48acff99ae1ee16dcdc2847046bebe5
Brian Levin <bml201095@gmail.com>
2020-08-09 11:57:49 -0400

    created a get request whcih will get the information on the homepage

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,12 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
+// get route -> index
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+  });
 
+  
 /*
 
 var burger = require('../models/burger.js'); //variables which wil hold the libraries
