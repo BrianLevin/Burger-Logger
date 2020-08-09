500d0bba210c8bf593e819ad1f2fcc4559ebf34f
Brian Levin <bml201095@gmail.com>
2020-08-09 12:29:18 -0400

    created javascript  which shows the burger has been devoured

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,23 @@
+
+$(document).ready(function() {
+    
+    $(".devour-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var burger_id = $(this).children(".burger_id").val();
+      console.log(burger_id);
+      $.ajax({
+        method: "PUT",
+        url: "/burgers/" + burger_id
+      }).then(function(data) {
+        // reload page to display devoured burger in proper column
+        location.reload();
+      });
+  
+    });
+  });
+
+/*
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
     $(".change-eaten").on("click", function (event) {
@@ -44,3 +64,4 @@ $(function () {
 
 }
 );
+*/
