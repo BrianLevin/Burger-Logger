69c42e56dd1ef6f14ab787d6ec7bccb674b80114
Brian Levin <bml201095@gmail.com>
2020-08-09 12:17:27 -0400

    created the seed.sql to store the intial values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burger (name) VALUES ('cheese_burger');
-INSERT INTO burger (name) VALUES ('double_burger');
-INSERT INTO burger (name) VALUES ('regular_burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
