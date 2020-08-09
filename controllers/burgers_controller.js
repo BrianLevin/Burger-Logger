b5ff19b1b91dd3c02f50fd672b6cd7a11c6d162e
Brian Levin <bml201095@gmail.com>
2020-08-09 12:02:23 -0400

    created the route which will get the burgers data

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,8 +8,14 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res) {
     res.redirect("/burgers");
   });
+  router.get("/burgers", function(req, res) {
+    // express callback response by calling burger.selectAllBurger
+    burger.all(function(burgerData) {
+      // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+      res.render("index", { burger_data: burgerData });
+    });
+  });
 
-  
 /*
 
 var burger = require('../models/burger.js'); //variables which wil hold the libraries
